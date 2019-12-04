553fda92f3e82ed5dca44c18ce7606a2a67dc119
Kyle Corbin <kylecorbin26@gmail.com>
2019-12-04 13:56:06 -0500

    Got all of the things working

--- server.js
+++ server.js
@@ -7,25 +7,43 @@ const server = express();
 // server.use(helmet())
 server.use(express.json());
 
+
 server.use('/api/hubs', hubsRouter);
 
-// custome middleware
+// custome middleware///////////////////////////////////////////
 const logger = (req,res,next)=>{
   console.log(`${req.method} to ${req.originalUrl}`);
 
   next();
 }
 
+server.use(logger);
+
 // gatekeeper that looks for password melon, if not send back status 401.message
   function gatekeeper(req,res,next){
   const password = req.headers.password;
 
-  if(password === "melon"){
+  if(password && password.toLowerCase() === "mellon"){
     next();
   }else{
     res.status(401).json({message: 'bad password in header'})
   }
 }
+
+//middleware will check for a role('admin'), role('agents')
+//when it take a paremeter is needs to return the three brothers function
+function checkRole(role){
+
+  return function(req,res,next){
+    if(role && role.toLowerCase() === req.headers.role){
+      next();
+    }else{
+      res.status(403).json({message: 'sorry you dont have access'})
+    }
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////////
 
 server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
@@ -40,9 +58,10 @@ server.get('/echo', (req,res)=>{
   res.send(req.headers);
 })
 
-server.get('/secret', helmet(), gatekeeper(), (req,res)=>{
+server.get('/secret', helmet(), gatekeeper, checkRole('admin'), (req,res)=>{
   res.send(req.headers);
 })
 
 
 module.exports = server;
+
